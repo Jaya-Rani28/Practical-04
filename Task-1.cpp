6e1939f87c878e47457ba9ce72eb727f33c2347b
Jaya-Rani28 <91416214+Jaya-Rani28@users.noreply.github.com>
2021-10-05 20:00:10 +0530

    Add files via upload

---
+++ Task-1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+  namespace first{
+  int add(int a, int b){
+  return a+b;
+  }
+  }
+  namespace second{
+   float add(float a, float b){
+   return a+b;
+   }
+   }
+   int main(){
+   cout<<first::add(1,2)<<"\n";
+   cout<<second::add(1.2,2)<<"\n";
+   cout<<second::add(1,2.1)<<"\n";
+   cout<<second::add(1.1,2.1)<<"\n";
+   cout<<second::add(0,0)<<"\n";
+   return 0;
+   }
